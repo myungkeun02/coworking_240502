4075692dd0b868088619c6f8eec758e3ceda84d1
myungkeun <audrms3568@gmail.com>
2024-05-02 23:58:46 +0900

    database docker setting

---
+++ src/main/java/org/myungkeun/coworking240502/Coworking240502Application.java
@@ -0,0 +1,13 @@
+package org.myungkeun.coworking240502;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coworking240502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coworking240502Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/myungkeun/coworking240502/Coworking240502ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myungkeun.coworking240502;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coworking240502ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
